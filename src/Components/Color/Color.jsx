583d182bb85a87084e55a6f70e0f528d64204c0d
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-06-26 10:36:21 +0200

    added confirmation request upon delete

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,24 @@
 import "./Color.css";
+import { useState } from "react";
 
 export default function Color({ color, onDeleteColor }) {
+  const [isConfirmingDelete, setIsConfirmingDelete] = useState(false);
+
+  const handleDeleteConfirmation = () => {
+    setIsConfirmingDelete(true); // Show confirmation
+  };
+
+  // Cancel deletion
+  const handleCancelDelete = () => {
+    setIsConfirmingDelete(false); // Hide confirmation
+  };
+
+  // Proceed with the deletion
+  const handleConfirmDelete = () => {
+    onDeleteColor(color.id);
+    setIsConfirmingDelete(false); // Hide confirmation
+  };
+
   return (
     <div
       className="color-card"
@@ -12,7 +30,18 @@ export default function Color({ color, onDeleteColor }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-      <button onClick={() => onDeleteColor(color.id)}>Delete Color</button>
+
+      {isConfirmingDelete ? (
+        <div>
+          <div className="delete-confirmation">Delete color?</div>
+          <button onClick={handleConfirmDelete}>Yes</button>
+          <button onClick={handleCancelDelete}>Cancel</button>
+        </div>
+      ) : (
+        <button onClick={handleDeleteConfirmation}>Delete</button>
+      )}
+
+      {/* <button onClick={() => onDeleteColor(color.id)}>Delete Color</button> */}
     </div>
   );
 }
